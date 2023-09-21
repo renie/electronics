fbc00fe5075a4198c382cd3802de89af496db3f3
Renie Siqueira <renie.webdev@gmail.com>
2023-09-21 18:03:49 +0300

    feat: create remaining unit tests

--- src/utils/strings.rs
+++ src/utils/strings.rs
@@ -4,3 +4,34 @@ pub fn remove_crlf(mut input: String) -> String {
 
     return input;
 }
+
+
+#[cfg(test)]
+mod tests {
+    use super::remove_crlf;
+
+    #[test]
+    fn remove_crlf_n_to_be_removed() {
+        assert_eq!("foo", remove_crlf(String::from("foo\n")));
+    }
+
+    #[test]
+    fn remove_crlf_r_to_be_removed() {
+        assert_eq!("foo", remove_crlf(String::from("foo\r")));
+    }
+
+    #[test]
+    fn remove_crlf_rn_to_be_removed() {
+        assert_eq!("foo", remove_crlf(String::from("foo\r\n")));
+    }
+
+    #[test]
+    fn remove_crlf_nr_to_be_removed() {
+        assert_eq!("foo", remove_crlf(String::from("foo\n\r")));
+    }
+
+    #[test]
+    fn remove_crlf_nothing_to_be_removed() {
+        assert_eq!("foo", remove_crlf(String::from("foo")));
+    }
+}
